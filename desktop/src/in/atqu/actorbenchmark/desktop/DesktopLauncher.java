065a648df16b0860ee04cd870e0e770ed64c93fc
Stafford Williams <stafford.williams@gmail.com>
2016-05-31 13:11:05 +1000

    project generated

---
+++ desktop/src/in/atqu/actorbenchmark/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package in.atqu.actorbenchmark.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import in.atqu.actorbenchmark.ActorBenchmark;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ActorBenchmark(), config);
+	}
+}
